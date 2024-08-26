b1562bc6326cdd86230d4c4de67a57691a324bc8
Mazzoni <mazzoni@gmx.fr>
2024-08-26 10:37:36 +0200

    up

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Pages\Renderer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController
 {
-    public function index($request): Response
+    public function index(Request $request): Response
     {
         return Renderer::render_template($request);
     }
--- src/main.php
+++ src/main.php
@@ -11,28 +11,37 @@ use App\Controller\HelloController;
 use App\Pages\Renderer;
 use App\Controller\FormController;
 use App\Controller\ByeController;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 
 $request = Request::createFromGlobals();
 
 $routes = new RouteCollection();
 
 $routes->add('hello', new Route('/hello', [
-    '_controller' => [new HelloController(), 'index']
+    '_controller' => 'App\Controller\HelloController::index'
 ]));
 $routes->add('form', new Route('/form', [
-    '_controller' => [new FormController(), 'index']
+    '_controller' => 'App\Controller\FormController::index'
 ]));
 $routes->add('bye', new Route('/bye', [
-    '_controller' => [new ByeController(), 'index']
+    '_controller' => 'App\Controller\ByeController::index'
 ]));
 
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
 try {
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    $response = call_user_func($request->attributes->get('_controller'), $request);
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
